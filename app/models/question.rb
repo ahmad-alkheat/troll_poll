6e86de05150160c710578aa32b98d07c45e89aa9
Ahmad <wisamfaithful@gmail.com>
2015-03-27 12:39:21 -0400

    generated answers model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :poll
+  has_many :possible_answers
   delegate :title, prefix: :true, to: :poll
   validates_presence_of :title
   validates_inclusion_of :kind, in: ApplicationHelper::QUESTION_KINDS
--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -3,4 +3,5 @@ require 'test_helper'
 class QuestionTest < ActiveSupport::TestCase
   should validate_presence_of(:title)
   should validate_inclusion_of(:kind).in_array(ApplicationHelper::QUESTION_KINDS)
+  should have_many :possible_answers
 end
